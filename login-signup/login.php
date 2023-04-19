61cc951056336ea5fb241569720d8a3ab92396c1
mstafamajid <mstafa.00857387@gmail.com>
2023-04-19 16:20:34 +0300

    create pages and dummy user

---
+++ login-signup/login.php
@@ -0,0 +1,65 @@
+<?php
+//TODO: getting username/email and passwords to auth
+//TODO: getting data if the user that have account
+session_start();
+$signInDataAsWriter = array('name' => "mustafa majid", 'username' => "mustafa", 'email' => "mstafa@gmail.com", 'password' => "mstafa123", 'type' => "writer");
+$signInDataAsReader = array('name' => "blnd zyad", 'username' => "blnd", 'email' => "blnd@gmail.com", 'password' => "blnd123", 'type' => "reader");
+$error = "";
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+    if (
+        $username == $signInDataAsWriter['username'] && $password == $signInDataAsWriter['password']
+    ) {
+        header('location: signup.php');
+        $_SESSION["userdata"] = $signInDataAsWriter;
+    } else if ($username == $signInDataAsReader['username'] && $password == $signInDataAsReader['password']) {
+
+        header('location: signup.php');
+        $_SESSION["userdata"] = $signInDataAsReader;
+    } else {
+        $error = "Incorrect username or password.";
+    }
+}
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <h2>Login Page</h2>
+        <form action="login.php" method="POST">
+            <label for="username">Username:</label>
+            <input type="text" name="username"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" name="password"><br><br>
+            <input type="checkbox" name="remember_me" value="checked"> Remember Me<br><br>
+            <input type="submit" value="Login"><br><br>
+    </div>
+    <div>
+        <a href="signup.php">Sign up now</a>
+    </div>
+
+    <?php
+    if (!empty($error))
+        echo " <span >$error</span>"
+    ?>
+    </form>
+</body>
+
+</html>
---
+++ login-signup/signup.php
@@ -0,0 +1,91 @@
+<?php
+//TODO: getting usernames and emails to check that if that username and email is used or not
+//TODO: validation for email field
+//TODO: sending data to databse to store
+
+$nameError = "";
+$usernameError = "";
+$emailError = "";
+$passwordError = "";
+$confirmPasswordError = "";
+$signupData = array('name' => "", 'username' => "", 'email' => "", 'password' => "", 'type' => "");
+
+if (isset($_POST["submit"])) {
+    if (empty($_POST["name"]))
+        $nameError = "please enter your name";
+    else $signupData['name'] = $_POST["name"];
+    if (empty($_POST["username"]))
+        $usernameError = "please enter your username";
+    else $signupData['username'] = $_POST["username"];
+    if (empty($_POST["email"]))
+        $emailError = "please enter your email";
+    else $signupData['name'] = $_POST["name"];
+    if (empty($_POST["password"]))
+        $passwordError = "please enter your password";
+    else $signupData['name'] = $_POST["name"];
+    if (empty($_POST["confirm-password"]))
+        $confirmPasswordError = "please enter your password";
+    else {
+        if (hash_equals($_POST["confirm-password"], $_POST["password"]))
+            $signupData["password"] = $_POST['password'];
+        else $confirmPasswordError = "please enter the same password";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="signup.php" method="POST">
+        <input type="text" id="name" name="name" placeholder="Enter your name\"><br>
+
+        <?php
+        if (!empty($nameError)) echo "<span class='error-input-signup'>$nameError</span>";
+
+        ?>
+        <br>
+        <input type="text" id="username" name="username" placeholder="Choose a username"><br>
+        <?php
+        if (!empty($usernameError)) echo "<span class='error-input-signup'>$usernameError</span>";
+
+        ?><br>
+
+        <input type="email" id="email" name="email" placeholder="Enter your email"><br>
+        <?php
+        if (!empty($emailError)) echo "<span class='error-input-signup'>$emailError</span>";
+
+        ?><br>
+
+        <input type="password" id="password" name="password" placeholder="Enter a password"><br>
+        <?php
+        if (!empty($passwordError)) echo "<span class='error-input-signup'>$passwordError</span>";
+
+        ?><br>
+
+        <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password"><br>
+        <?php
+        if (!empty($confirmPasswordError)) echo "<span class='error-input-signup'>$confirmPasswordError</span>";
+
+        ?><br>
+
+        <input type="radio" id="writer" name="role" value="writer">
+        <span>Writer</span>
+
+        <input type="radio" id="reader" name="role" value="reader" checked>
+        <span>Reader</span><br><br>
+
+        <input type="submit" value="Create Account" name="submit">
+    </form>
+
+</body>
+
+</html>
