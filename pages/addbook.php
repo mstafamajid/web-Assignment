c2c129152af8f9527ea83a97b79e6af2f3d54a1a
mstafamajid <mstafa.00857387@gmail.com>
2023-04-22 22:57:52 +0300

    fix warning

--- pages/addbook.php
+++ pages/addbook.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 $logoPath = "../assets\logo.svg";
 include '../includes/navbar.php';
 
 
-if(isset($_FILES['image'])) {
+if (isset($_FILES['image'])) {
 
-    if($_FILES['image']['error'] == 0) {
-    
+    if ($_FILES['image']['error'] == 0) {
+
         $file_path = $_FILES['image']['tmp_name'];
         $file_name = $_FILES['image']['name'];
 
 
-      
-        move_uploaded_file($file_path,"../uploads/".$file_name);
-        $image_path = "uploads/". $file_name;
- exit();
-    } 
+
+        move_uploaded_file($file_path, "../uploads/" . $file_name);
+        $image_path = "uploads/" . $file_name;
+        exit();
+    }
 }
 
 
@@ -23,28 +23,31 @@ if(isset($_FILES['image'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body{
-           background-color:  #ebebeb;
-        
+        body {
+            background-color: #ebebeb;
+
         }
-        .main{
-          
+
+        .main {
+
             margin: 0 auto;
             display: flex;
             flex-direction: column;
-align-items: center;
+            align-items: center;
 
-background-color: white;
-width: 400px;
-height: 500px;
+            background-color: white;
+            width: 400px;
+            height: 500px;
         }
-        form{
+
+        form {
             padding: 0px 30px;
             gap: 30px;
             display: flex;
@@ -52,26 +55,25 @@ height: 500px;
             align-items: center;
             justify-content: center;
         }
-        input[type="file"]{
-           
-        }
-        .upload-cover{
+
+        .upload-cover {
             height: 60px;
             width: 100%;
-        
-          border: 2px dashed black;
-          border-radius: 20px;
+
+            border: 2px dashed black;
+            border-radius: 20px;
             display: flex;
             flex-direction: row;
-        align-items: center;
-        justify-content: space-evenly;
+            align-items: center;
+            justify-content: space-evenly;
         }
-        .icon{
+
+        .icon {
             display: flex;
-            
-            justify-content: center; 
-  align-items: center; 
-         padding: 0 10px;
+
+            justify-content: center;
+            align-items: center;
+            padding: 0 10px;
             height: 50px;
             background-color: white;
             border-radius: 10px;
@@ -79,22 +81,24 @@ height: 500px;
         }
     </style>
 </head>
+
 <body>
     <div class="main">
         <h2>Add Book</h2>
         <form action='' method="post" enctype="multipart/form-data">
-<input type="text" name="book-name" id="book-name">
-<textarea name="book-desc" id="book desc" cols="40" rows="10"></textarea>
-<div class="upload-cover">
-    <div class="icon">
-<img src="../assets/addbook.svg" alt="">
-    </div>
-    <h4>upload book photo</h4>
-<input type="file" name="image" id="upload" >
+            <input type="text" name="book-name" id="book-name">
+            <textarea name="book-desc" id="book desc" cols="40" rows="10"></textarea>
+            <div class="upload-cover">
+                <div class="icon">
+                    <img src="../assets/addbook.svg" alt="">
+                </div>
+                <h4>upload book photo</h4>
+                <input type="file" name="image" id="upload">
 
-</div>
-<input type="submit" value="add">
+            </div>
+            <input type="submit" value="add">
         </form>
     </div>
 </body>
+
 </html>
