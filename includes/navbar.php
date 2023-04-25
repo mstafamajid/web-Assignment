7a7fe000c344e08a7fe75c1b38b03202eef90f5a
Abdulbari <abdulbari.112917@gmail.com>
2023-04-25 22:03:02 +0300

    responsive finished

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,82 +6,95 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="../styles/navbar.css">
 </head>
 
 <body>
     <nav>
-            <div>
-                <?php echo " <img src='$logoPath' alt='logo'>" ?>
-            </div>
-            <div>
-                <ul>
-                    <li><a href="home.php">Home</a></li>
-                    <li><a href="books.php">Books</a></li>
-                    <li><a href="profile.php">Profile</a></li>
-                </ul>
-            </div>
-            <div id='srch-ic'>
-                <!-- search svg code -->
-                <div class="srch" >
-                
-                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 svg">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-</svg>
+        <div>
+            <?php echo " <img src='$logoPath' alt='logo'>" ?>
+        </div>
+        <div>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="books.php">Books</a></li>
+                <li><a href="profile.php">Profile</a></li>
+            </ul>
+        </div>
+        <div id='srch-ic'>
+            <!-- search svg code -->
+            <div class="srch">
 
-                </div>
-                <input type="search"  name="search" id="search-field">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 svg">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+
+            </div>
+            <input type="search" name="search" id="search-field">
+            <div>
+
+
+                <!-- image profile -->
                 <div>
 
-
-                <!-- image profile --> 
-                <div>
-
                 </div>
 
             </div>
-        
+
     </nav>
 
 
-<!--   navbar for small screen   -->
+    <!--   navbar for small screen   -->
 
 
-<div class="topBar" >
+    <div class="topBar">
 
-<div class="logoM">
-    <a href="home.php"><img src="../assets/logoW.svg" alt="logo"></a>
-</div>
+        <div class="logoM">
+            <a href="home.php"><img src="../assets/logoW.svg" alt="logo"></a>
+        </div>
 
 
-<div class="pfpM">
-<img src="../assets/profile-pic.png" alt="php picture">
-</div>
+        <div class="pfpM">
+            <img src="../assets/profile-pic.png" alt="php picture">
+        </div>
 
-</div>
+    </div>
 
 
     <div class="mobNav">
 
 
 
-    <ul>
-        <li>
-            <a href="../pages/home.php"><img src="../assets/homeM.svg" alt=""></a>
-            Home
-       </li>
-        
-        <li>
-            <a href="../pages/books.php"><img src="../assets/bookM.svg" alt=""></a>
-            Books
-        </li>
-        
-        <li>
-            <a href="../pages/profile.php"><img src="../assets/pfpM.svg" alt=""></a>
-            Profile
-        </li>
-     </ul> 
-    
+        <ul>
+            <li>
+                <a class="a-nav__con" href="../pages/home.php">
+
+                    <img src="../assets/homeM.svg" alt="">
+                    <span>Home</span></a>
+
+            </li>
+
+            <li>
+                <a class="a-nav__con" href="../pages/books.php">
+
+                    <img src="../assets/bookM.svg" alt="">
+                    <span>Books</span></a>
+
+            </li>
+
+            <li>
+                <a class="a-nav__con" href="../pages/profile.php">
+
+                    <img src="../assets/pfpM.svg" alt="">
+                    <span>
+                        Profile
+                    </span></a>
+
+            </li>
+        </ul>
+
 
 
 
--- pages/profile.php
+++ pages/profile.php
@@ -26,6 +26,7 @@ $bookdata = array(
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -34,48 +35,53 @@ $bookdata = array(
     <link rel="stylesheet" href="../styles/profile.css">
     <!-- google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&family=Poppins:wght@400;600&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
-   <div class="main">
-    <!-- left side  -->
-    <div class="sideProfile">
-<div class="imageprofile">
-<img class="img" src="../assets/blank-profile-picture-hd-images-photo.JPG" alt="profile image">
+    <div class="main">
+        <!-- left side  -->
+        <div class="sideProfile">
+            <div class="imageprofile">
+                <img class="img" src="../assets/blank-profile-picture-hd-images-photo.JPG" alt="profile image">
 
-</div>
-<div class="userinfo">
+            </div>
+            <div class="userinfo">
 
-<!-- to retrive the name you just need put this code inside php tag -->
-<!-- php echo $userdata['']  -->
-<h2>name</h2>
+                <!-- to retrive the name you just need put this code inside php tag -->
+                <!-- php echo $userdata['']  -->
+                <h2>name</h2>
 
-<!-- to retrive the username you just need put this code inside php tag -->
-<!-- php echo "@".$userdata[''] -->
-<h4 class="username">@userName</h4>
-</div>
-<div class="bookinfo">
-<div class="numbook ">
-    <h2 class=" num">No. books</h2>
-    <h2><?php echo $userdata['num-of-book']?></h2>
-</div>
-<div class="numpost">
-    <h2 class=" num">No. posts</h2>
-    <h2><?php echo $userdata['num-of-post']?></h2>
-</div>
-</div>
-
-    </div>
-
-    <!--right side -->
+                <!-- to retrive the username you just need put this code inside php tag -->
+                <!-- php echo "@".$userdata[''] -->
+                <h4 class="username">@userName</h4>
+            </div>
+            <div class="bookinfo">
+                <div class="numbook ">
+                    <h2 class=" num">No. books</h2>
+                    <h2><?php echo $userdata['num-of-book']?></h2>
+                </div>
+                <div class="numpost">
+                    <h2 class=" num">No. posts</h2>
+                    <h2><?php echo $userdata['num-of-post']?></h2>
+                </div>
 
 
-    <div class="booksContainar">
-        <h2>books</h2>
+            </div>
+            <button class="btn-add"><a href="">Add book</a> </button>
+            <button class="btn-add"><a href=""> Add post</a></button>
+        </div>
 
-<div class="books">
-<?php 
+        <!--right side -->
+
+
+        <div class="booksContainar">
+            <h2>books</h2>
+
+            <div class="books">
+                <?php 
 for ($i=0; $i < count($bookdata); $i++) { 
   echo "  <div class='eachBook'>
   <div class='picture'>
@@ -88,9 +94,10 @@ for ($i=0; $i < count($bookdata); $i++) {
 </div>";
 }
 ?>
-</div>
-      
+            </div>
+
+        </div>
     </div>
-   </div>
 </body>
+
 </html>
