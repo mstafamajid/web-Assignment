6ff2f34febbb30ac63340fa956cac9ae50cbe16b
Blnd <89999251+Blnd-z@users.noreply.github.com>
2023-05-02 18:32:13 +0300

    Add files via upload

---
+++ pages/aboutUs.php
@@ -0,0 +1,67 @@
+<?php 
+
+$logoPath = "../assets\logo.svg";
+include '../includes/navbar.php';
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="aboutUs.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>About Us</h1>
+
+<div class="container">
+
+
+<div class="card">
+    <img src="../assets/blnd.jpg" alt="blnd img">
+    <h2>Blnd Zyad</h2>
+    <p>Ui\Ux Designer</p>
+</div>
+
+<div class="card">
+    <img src="../assets/mustafa.jpg" alt="Mustafa img">
+    <h2>Mustafa Majid</h2>
+    <p>Flutter Developer</p>
+</div>
+
+
+</div>
+
+
+<footer>
+
+<img src="../assets/logoW.svg" alt="">
+
+<div class="links">
+<a href="">Email</a>
+<a href="">Facebook</a>
+<a href="">Linked In</a>
+</div>
+
+<hr>
+
+<div class="bot">
+<p class="tell">0750 xxx xxxx  -  0750 xxx xxxx</p>
+<p class="coppyR">© 2023 KoyaUniveristy. All rights reserved.</p>
+</div>
+
+</footer>
+
+
+
+
+</body>
+</html>
